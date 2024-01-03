cf373beebace71b9aeaabbee53e9be8c9757ffe6
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-01-03 10:17:20 +0100

    Fix cross compile
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- libafb/capi/build.rs
+++ libafb/capi/build.rs
@@ -30,7 +30,7 @@ fn main() {
         // -----------------------------------------------------------------------
         ";
     // probe for dependencies
-    #[cfg(feature="rpm_build")]
+    #[cfg(not(feature="rpm_build"))]
     system_deps::Config::new().probe().unwrap();
 
     // invalidate the built crate whenever the wrapper changes
