c7cfc5b6030a42aa80c35d5d07aa49500284e3d3
Hugo Mercier <hugo.mercier@iot.bzh>
2024-07-29 13:58:15 +0200

    Remove a compilation warning
    Signed-off-by: Hugo Mercier <hugo.mercier@iot.bzh>

--- afb-librs/capi/cglue-mod.rs
+++ afb-librs/capi/cglue-mod.rs
@@ -25,6 +25,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+#![allow(unused_imports)]
 include!("_libafb-map.rs");
 
 // hack to force RUST to export afbBinding mandatory entry points
